f67f9201dd4b17358a00aa919308c7aa82a27b8b
pbeitia <78168656+pbeitia@users.noreply.github.com>
2021-03-03 19:51:11 +0100

    Soluciono errores en SumaTest y + casos especiales

--- SumaTest.java
+++ SumaTest.java
@@ -1,14 +1,16 @@
+//Importo las clases necesarias
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+//Creo la clase SumaTest para realizar las pruebas
 class SumaTest {
 	
+	//Preparo las pruebas
 	private Suma sum;
 	
-	@BeforeEach //PREPARAR
+	@BeforeEach
 	public void setup() {
 		sum = new Suma();
 	}
@@ -19,8 +21,9 @@ class SumaTest {
 	}
 	
 	//SUMA TIPO UNO-------------------------------------------------------
-	@Test
-	void laSumaDe2Más2Es4Double() {
+	
+	@Test //Prueba Normal
+	void laSumaDe2Mas2Es4Double() {
 		//1-PREPARACIÓN
 		double num1 = 2;
 		double num2 = 2;
@@ -33,8 +36,8 @@ class SumaTest {
 		assertEquals(esperando, obtenido,"El resultado debe ser 4");
 	}
 	
-	@Test 
-	void laSumaDeMenos2Más2Es0Double() {
+	@Test //Si algun numero es negativo, la suma se realiza igual.
+	void laSumaDeMenos2Mas2Es0Double() {
 		//1-PREPARACIÓN
 		double num1 = -2;
 		double num2 = 2;
@@ -47,8 +50,8 @@ class SumaTest {
 		assertEquals(esperando, obtenido,"El resultado debe ser 0");
 	}
 	
-	@Test
-	void laSumaDe0Más2coma2Es2coma2Double() {
+	@Test //Si algun numero es 0, la suma se realiza de la misma forma.
+	void laSumaDe0Mas2coma2Es2coma2Double() {
 		//1-PREPARACIÓN
 		double num1 = 0;
 		double num2 = 2.2;
@@ -61,8 +64,8 @@ class SumaTest {
 		assertEquals(esperando, obtenido,"El resultado debe ser 2.2");
 	}
 	
-	@Test
-	void laSumaDe2coma2Más2coma2Es4coma4Double() {
+	@Test //Si algun numero es decimal, la suma se realiza de la misma forma.
+	void laSumaDe2coma2Mas2coma2Es4coma4Double() {
 		//1-PREPARACIÓN
 		double num1 = 2.2;
 		double num2 = 2.2;
@@ -75,10 +78,25 @@ class SumaTest {
 		assertEquals(esperando, obtenido,"El resultado debe ser 4.4");
 	}
 	
+	@Test //Si algun numero es demasiado alto da error.
+	void laSumaDe999999999Mas2Es4ErrorDouble() {
+		//1-PREPARACIÓN
+		double num1 = 999999999;
+		double num2 = 2;
+		double esperando = 0;
+		
+		//2-EJECUCIÓN
+		double obtenido = sum.sumaTipoUno(num1,num2);
+		
+		//3-COMPROBAR
+		assertEquals(esperando, obtenido,"El resultado debe ser ERROR");
+	}
+	
+	
 	//SUMA TIPO DOS-------------------------------------------------------
 	
-	@Test
-	void laSumaDe2Más2Es4Int() {
+	@Test //Prueba Normal
+	void laSumaDe2Mas2Es4Int() {
 		//1-PREPARACIÓN
 		int num1 = 2;
 		int num2 = 2;
@@ -91,10 +109,10 @@ class SumaTest {
 		assertEquals(esperando, obtenido,"El resultado debe ser 4");
 	}
 	
-	@Test
-	void laSumaDeMenos2Más2EsErrorInt() {
+	@Test //Si algun numero es negativo, devuelve el resultado.
+	void laSumaDeMenos2Mas2Es0Int() {
 		//1-PREPARACIÓN
-		int num1 = 2;
+		int num1 = -2;
 		int num2 = 2;
 		int esperando = 0;
 		
@@ -102,11 +120,11 @@ class SumaTest {
 		int obtenido = sum.sumaTipoDos(num1, num2);
 		
 		//3-COMPROBAR
-		assertEquals(esperando, obtenido,"El resultado debe ser ERROR");
+		assertEquals(esperando, obtenido,"El resultado debe ser 0");
 	}
 	
-	@Test
-	void laSumaDe0Más2Es2Int() {
+	@Test //Si algun numero es 0, la suma se realiza de la misma forma.
+	void laSumaDe0Mas2Es2Int() {
 		//1-PREPARACIÓN
 		int num1 = 0;
 		int num2 = 2;
@@ -119,8 +137,8 @@ class SumaTest {
 		assertEquals(esperando, obtenido,"El resultado debe ser 2");
 	}
 	
-	@Test
-	void laSumaDe2coma2Más0EsErrorInt() {
+	@Test //Si algun numero es decimal, la suma no se realizaria porque es con numeros enteros.
+	void laSumaDe2coma2Mas0EsErrorInt() {
 		//1-PREPARACIÓN
 		int num1 = 2.2;
 		int num2 = 0;
@@ -133,9 +151,25 @@ class SumaTest {
 		assertEquals(esperando, obtenido,"El resultado debe ser ERROR");
 	}
 	
+	@Test //Si algun numero es demasiado alto da error.
+	void laSumaDe999999999Mas2EsErrorInt() {
+		//1-PREPARACIÓN
+		int num1 = 999999999;
+		int num2 = 2;
+		int esperando = 0;
+		
+		//2-EJECUCIÓN
+		int obtenido = sum.sumaTipoDos(num1,num2);
+		
+		//3-COMPROBAR
+		assertEquals(esperando, obtenido,"El resultado debe ser ERROR");
+	}
+	
+	
 	//SUMA TIPO TRES-------------------------------------------------------
+	
 	@Test
-	void laSumaDe2Más2Más2Es6Double() {
+	void laSumaDe2Mas2Mas2Es6Double() {
 		//1-PREPARACIÓN
 		double num1 = 2;
 		double num2 = 2;
@@ -149,7 +183,7 @@ class SumaTest {
 	}
 	
 	@Test
-	void laSumaDe2coma2Más2Más2Es6coma2Double() {
+	void laSumaDe2coma2Mas2Mas2Es6coma2Double() {
 		//1-PREPARACIÓN
 		double num1 = 2.2;
 		double num2 = 2;
@@ -163,7 +197,7 @@ class SumaTest {
 	}
 	
 	@Test
-	void laSumaDe0Más2Más2Es4Double() {
+	void laSumaDe0Mas2Mas2Es4Double() {
 		//1-PREPARACIÓN
 		double num1 = 0;
 		double num2 = 2;
@@ -177,7 +211,7 @@ class SumaTest {
 	}
 	
 	@Test
-	void laSumaDeMenos2Más2Más2Es2Double() {
+	void laSumaDeMenos2Mas2Mas2Es2Double() {
 		//1-PREPARACIÓN
 		double num1 = -2;
 		double num2 = 2;
@@ -190,8 +224,25 @@ class SumaTest {
 		assertEquals(esperando, obtenido,"El resultado debe ser 2");
 	}
 	
+	@Test //Si algun numero es demasiado alto da error.
+	void laSumaDe999999999Mas2MasEsErrorDouble() {
+		//1-PREPARACIÓN
+		double num1 = 999999999;
+		double num2 = 2;
+		double num3 = 2;
+		double esperando = 0;
+		
+		//2-EJECUCIÓN
+		double obtenido = sum.sumaTipoTres(num1, num2, num3);
+		
+		//3-COMPROBAR
+		assertEquals(esperando, obtenido,"El resultado debe ser ERROR");
+	}
+	
+	
 	//SUMA TIPO CUATRO-------------------------------------------------------
-	@Test
+	
+	@Test //Prueba Normal
 	void laAcumulaciónDe2Es2Double() {
 		//1-PREPARACIÓN
 		double num = 2;
@@ -202,7 +253,7 @@ class SumaTest {
 		assertEquals(esperando, obtenido,"El resultado debe ser 2");
 	}
 	
-	@Test
+	@Test //Si algun numero es negativo, se sumaria al acumulador.
 	void laAcumulaciónDeMenos2EsMenos2Double() {
 		//1-PREPARACIÓN
 		double num = -2;
@@ -213,7 +264,7 @@ class SumaTest {
 		assertEquals(esperando, obtenido,"El resultado debe ser -2");
 	}
 	
-	@Test
+	@Test //Si algun numero es 0, sumaria al acumulador.
 	void laAcumulaciónDe0Es0Double() {
 		//1-PREPARACIÓN
 		double num = 0;
@@ -224,7 +275,7 @@ class SumaTest {
 		assertEquals(esperando, obtenido,"El resultado debe ser 0");
 	}
 	
-	@Test
+	@Test //Si algun numero es decimal, sumaria al acumulador.
 	void laAcumulaciónDe2coma2Es2coma2Double() {
 		//1-PREPARACIÓN
 		double num = 2.2;
